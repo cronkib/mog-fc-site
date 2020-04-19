ff2b080e3d4ca98c771a8859e757f133645bb49a
Travis Kern <kern.travis@gmail.com>
2020-04-19 15:48:57 -0400

    Fully rename project to mog-fc-client

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('freecompany-client app is running!');
+    expect(page.getTitleText()).toEqual('mog-fc-site app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'freecompany-client'`, () => {
+  it(`should have as title 'mog-fc-site'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('freecompany-client');
+    expect(app.title).toEqual('mog-fc-site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('freecompany-client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mog-fc-site app is running!');
   });
 });
